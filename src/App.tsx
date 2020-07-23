da97a8b2478ce6723062c3a986d8d8366a34eae3
Zharrazh <37090840+Zharrazh@users.noreply.github.com>
2020-07-23 11:15:42 +0300

    настройки

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
+import { Header } from "./Components/Header";
+import { Content } from "./Components/Content";
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Content />
     </div>
   );
 }
